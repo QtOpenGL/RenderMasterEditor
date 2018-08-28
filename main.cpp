473e41c4187a8dc8eb9d82ca5e56bb50a754c2b4
Konstantin <consta@unigine.com>
2018-08-28 13:34:34 +0700

    Title

--- main.cpp
+++ main.cpp
@@ -57,7 +57,9 @@ int main(int argc, char *argv[])
     QWidget *render_view = w->findChild<QWidget*>("D3D11Widget");
     HWND h = (HWND)render_view->winId();
 
-	auto rm_inited =pCore->Init(INIT_FLAGS::EXTERN_WINDOW | INIT_FLAGS::OPENGL45, "resources", &h);
+	auto glFlag = INIT_FLAGS::OPENGL45;
+	w->setWindowTitle(w->windowTitle() + (glFlag == INIT_FLAGS::OPENGL45 ? "  (GL)" : " (DX11)"));
+	auto rm_inited =pCore->Init(INIT_FLAGS::EXTERN_WINDOW | glFlag, "resources", &h);
 
 	if (SUCCEEDED(rm_inited))
 	{
