594bc8722d15b1c9240ea02e8fa169d53de25273
Konstantin <consta@unigine.com>
2018-12-16 03:33:33 +0800

    1

--- manipulators/ManipulatorTranslator.cpp
+++ manipulators/ManipulatorTranslator.cpp
@@ -10,6 +10,11 @@ extern EngineGlobal* eng;
 
 const float SelectDistance = 8.0f;
 const float MaxDistance = 1000000.0f;
+const vec3 AxesEndpointDelta[3] = {vec3(1, 0, 0), vec3(0, 1, 0), vec3(0, 0, 1)};
+const vec4 ColorSelection = vec4(1,1,0,1);
+const vec4 ColorRed = vec4(1,0,0,1);
+const vec4 ColorGreen = vec4(0,1,0,1);
+const vec4 ColorBlue = vec4(0,0,1,1);
 
 ManipulatorTranslator::~ManipulatorTranslator()
 {
@@ -20,7 +25,7 @@ bool ManipulatorTranslator::isMouseIntersects(const vec2& normalizedMousePos)
 	return moiseHoverAxis != AXIS::NONE;
 }
 
-float axisSize(uint h, float dist)
+float axisWorldSize(uint h, float dist)
 {
 	return (85.0f / h) * dist;
 }
@@ -33,9 +38,9 @@ void ManipulatorTranslator::update(ICamera *pCamera, const QRect &screen, IRende
 
 
 	// camera
-	mat4 camVP;
+	mat4 camViewProj;
 	float aspect = (float)w / h;
-	pCamera->GetViewProjectionMatrix(&camVP, aspect);
+	pCamera->GetViewProjectionMatrix(&camViewProj, aspect);
 
 	mat4 camModelMatrix;
 	pCamera->GetModelMatrix(&camModelMatrix);
@@ -51,9 +56,9 @@ void ManipulatorTranslator::update(ICamera *pCamera, const QRect &screen, IRende
 	// selection
 	mat4 selectionWorldTransform = editor->GetSelectionTransform();
 
-	vec4 view4 = camVP * vec4(selectionWorldTransform.el_2D[0][3], selectionWorldTransform.el_2D[1][3], selectionWorldTransform.el_2D[2][3], 1.0f);
+	vec4 view4 = camViewProj * vec4(selectionWorldTransform.el_2D[0][3], selectionWorldTransform.el_2D[1][3], selectionWorldTransform.el_2D[2][3], 1.0f);
 	vec3 view(view4);
-	float dist = view.Lenght();
+	float distToSelction = view.Lenght();
 
 	mat4 axisTransform = editor->GetSelectionTransform();
 	vec3 axisOrigin = axisTransform.Column3(3);
@@ -61,7 +66,7 @@ void ManipulatorTranslator::update(ICamera *pCamera, const QRect &screen, IRende
 	vec3 V = (axisOrigin - cameraPosition).Normalized();
 
 
-	auto distance_to_axis = [&](const vec3& axis, const vec3& endPoint) -> float
+	auto distance_to_axis = [&](const vec3& axis, AXIS type) -> float
 	{
 		vec3 VcrossAxis = V.Cross(axis).Normalized();
 		vec3 N = axis.Cross(VcrossAxis).Normalized();
@@ -75,12 +80,12 @@ void ManipulatorTranslator::update(ICamera *pCamera, const QRect &screen, IRende
 		{
 		   //qDebug() <<"intersection x:" << vec3ToString(i);
 
-		   vec2 i = NdcToScreen(WorldToNdc(I, camVP), w, h);
-		   vec2 A = NdcToScreen(WorldToNdc(axisOrigin, camVP), w, h);
-		   vec4 axisEndpointLocal = vec4(endPoint * axisSize(h, dist));
+		   vec2 i = NdcToScreen(WorldToNdc(I, camViewProj), w, h);
+		   vec2 A = NdcToScreen(WorldToNdc(axisOrigin, camViewProj), w, h);
+		   vec4 axisEndpointLocal = vec4(AxesEndpointDelta[(int)type] * axisWorldSize(h, distToSelction));
 		   vec4 axisEndpointWorld = axisTransform * axisEndpointLocal;
-		   vec2 BNdc = WorldToNdc(axisEndpointWorld.Vec3(), camVP);
-		   vec2 B = NdcToScreen(BNdc, w, h);
+		   vec2 Bndc = WorldToNdc(axisEndpointWorld.Vec3(), camViewProj);
+		   vec2 B = NdcToScreen(Bndc, w, h);
 
 		   float dist = PointToSegmentDistance(A, B, i);
 
@@ -94,13 +99,12 @@ void ManipulatorTranslator::update(ICamera *pCamera, const QRect &screen, IRende
 
 
 	vec3 axes[3] = { axisTransform.Column3(0).Normalized(), axisTransform.Column3(1).Normalized(), axisTransform.Column3(2).Normalized() };
-	vec3 axesEndpointDelta[3] = { vec3(1, 0, 0), vec3(0, 1, 0), vec3(0, 0, 1) };
 	moiseHoverAxis = AXIS::NONE;
 	float minDist = MaxDistance;
 
 	for (int i = 0; i < 3; i++)
 	{
-		float dist = distance_to_axis(axes[i], axesEndpointDelta[i]);
+		float dist = distance_to_axis(axes[i], (AXIS)i);
 
 		if (dist < SelectDistance && dist < minDist)
 		{
@@ -143,9 +147,9 @@ void ManipulatorTranslator::render(RENDER_MASTER::ICamera *pCamera, const QRect&
 		pCoreRender->SetDepthTest(0);
 
 		mat4 distanceScaleMat;
-        distanceScaleMat.el_2D[0][0] = axisSize(h, dist);
-        distanceScaleMat.el_2D[1][1] = axisSize(h, dist);
-        distanceScaleMat.el_2D[2][2] = axisSize(h, dist);		
+		distanceScaleMat.el_2D[0][0] = axisWorldSize(h, dist);
+		distanceScaleMat.el_2D[1][1] = axisWorldSize(h, dist);
+		distanceScaleMat.el_2D[2][2] = axisWorldSize(h, dist);
 
 		auto draw_axis = [&](AXIS type, const vec4& color) -> void
 		{
@@ -173,14 +177,9 @@ void ManipulatorTranslator::render(RENDER_MASTER::ICamera *pCamera, const QRect&
 			pCoreRender->Draw(_pAxesArrowMesh);
 		};
 
-		static const vec4 colorSelection = vec4(1,1,0,1);
-		static const vec4 colorRed = vec4(1,0,0,1);
-		static const vec4 colorGreen = vec4(0,1,0,1);
-		static const vec4 colorBlue = vec4(0,0,1,1);
-
-		draw_axis(AXIS::X, (int)moiseHoverAxis == 0 ? colorSelection : colorRed);
-		draw_axis(AXIS::Y, (int)moiseHoverAxis == 1 ? colorSelection : colorGreen);
-		draw_axis(AXIS::Z, (int)moiseHoverAxis == 2 ? colorSelection : colorBlue);
+		draw_axis(AXIS::X, (int)moiseHoverAxis == 0 ? ColorSelection : ColorRed);
+		draw_axis(AXIS::Y, (int)moiseHoverAxis == 1 ? ColorSelection : ColorGreen);
+		draw_axis(AXIS::Z, (int)moiseHoverAxis == 2 ? ColorSelection : ColorBlue);
 
 		shader->Release();
 	}
